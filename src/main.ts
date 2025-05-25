52391c9f3b427c033bfbfa3aa850dc2e63baf5e8
sebasbit <sebastian.torres0426@gmail.com>
2025-05-25 17:57:44 -0500

    feat: add configuration module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = app.get(ConfigService).get<number>('app.port') ?? 3001;
+
+  await app.listen(port);
+
+  const logger = new Logger('boostrap');
+  logger.log(`Server (microsrv-payment) is listening on port ${port}`);
 }
 bootstrap();
