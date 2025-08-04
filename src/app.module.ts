6b16dcaa4dffbb0d08007e8007d9856c906a9c67
sebasbit <sebastian.torres0426@gmail.com>
2025-08-03 23:44:39 -0500

    feat: add health-check endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PaymentsModule } from './payments/payments.module';
+import { HealthCheckModule } from './health-check/health-check.module';
 import configuration from './config/configuration';
 import validationSchema from './config/env.validation';
 
@@ -12,6 +13,7 @@ import validationSchema from './config/env.validation';
       validationSchema,
     }),
     PaymentsModule,
+    HealthCheckModule,
   ],
   controllers: [],
   providers: [],
